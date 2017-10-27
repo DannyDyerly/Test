70933d83b785042b8912508db2541ed6c82bdd80
Daniel Dyerly-145003729 <145003729@vusdnoc.vusd.k12.ca.us>
2017-10-27 10:47:50 -0700

    added print statement

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         
         System.out.println("where is my super suit");
+        System.out.println("yee");
         
     }
     
