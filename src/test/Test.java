8877ca2ea4e55ae433803c9ab908ebb8c3a95081
Daniel Dyerly-145003729 <145003729@vusdnoc.vusd.k12.ca.us>
2017-10-30 11:17:39 -0700

    cleaned up

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,15 +4,7 @@ public class Test {
     
     public static void main(String[] args) {
         
-        System.out.println("where is my super suit");
-        System.out.println("yee");
-        System.out.println("illdoitlatermom");
-        //dasdfasdfasdfasdfasdf
-        //ljhsbdjhkasbdjfhbasjkdfha sjdf
-        //illdoitlatermom
-        //change
-        //nibber
-        //illdoitlaterdad
+    
         
     }
     
